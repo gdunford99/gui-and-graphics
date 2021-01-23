497bb5f5b334e00a395fa4d14c4a34b222ce0407
gdunford99 <56702202+gdunford99@users.noreply.github.com>
2021-01-23 14:25:24 -0800

    Update cone.js

--- Assignment_3/cone.js
+++ Assignment_3/cone.js
@@ -5,21 +5,21 @@ function init() {
     var canvas = document.getElementById( "webgl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
-    cone = new Cone (gl, n);
+    cone = new Cone (gl, 8);
     
     if ( !gl ) {
         alert("Unable to setup WebGL");
         return;
     }
 
-    gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
+    //gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
 
     render();
 }
 
 function render() {
     cone.render();
-    gl.clear( gl.COLOR_BUFFER_BIT );
+    //gl.clear( gl.COLOR_BUFFER_BIT );
     
 }
 
