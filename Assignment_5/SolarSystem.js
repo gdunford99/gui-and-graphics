4d303a8bcb4e147faaf985451e2e299990b8a297
gdunford99 <56702202+gdunford99@users.noreply.github.com>
2021-02-09 16:06:16 -0800

    Add files via upload

---
+++ Assignment_5/SolarSystem.js
@@ -0,0 +1,78 @@
+var SolarSystem = {
+  Sun : {
+    radius : 3,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.0553,
+    distance : 4,
+    year : 1.241,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 0.949,
+    distance : 5,
+    year : 1.615,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Earth : {
+    radius : 1,
+    distance : 7,
+    year : 1,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 0.2724,
+    distance : 2,
+    year : 1,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 0.532,
+    distance : 11,
+    year : 1.88,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 2.21,
+    distance : 15,
+    year : 1.9,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 1.0, 153/255, 0.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 1.75,
+    distance : 19.58,
+    year : 1.4,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 2.01,
+    distance : 23,
+    year : 1.9,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 0.0, 0.1, 1.0, 1.0 ]
+  },
+  Neptune : {
+    radius : 1.88,
+    distance : 27.05,
+    year : 1.7,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.186,
+    distance : 30.48,
+    year : 3.5,
+	axis : [0.0, 1.0, 0.0],
+    color : [ 150/255, 75/255, 0.0, 1.0 ]
+  }
+};
